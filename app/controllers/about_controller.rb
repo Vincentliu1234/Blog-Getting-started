c7a6f1ab40121b2dca8f28722137d1c6333fdde4
Vince <vincentliu1234@gmail.com>
2015-04-13 21:47:35 -0700

    second attempt at push

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,11 @@
+class AboutController < ApplicationController
+  
+  # This is a method defined within the AboutController
+  # This is called "Action"
+  # So the route:   get "/about" => "about#index"
+  # Will call this method
+  def index
+
+  end
+
+end
